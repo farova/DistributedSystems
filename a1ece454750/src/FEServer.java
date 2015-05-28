b625fc4c8d7d1f253fa879c8803cb855cc80b3b3
farova <michaela.farova@gmail.com>
2015-05-27 23:54:27 -0400

    Removing useles junk from files

--- a1ece454750/src/FEServer.java
+++ a1ece454750/src/FEServer.java
@@ -1,23 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
-// Generated code
 import ece454750s15a1.*;
 
 import org.apache.thrift.TException;
@@ -63,21 +44,5 @@ public class FEServer {
 		
 		System.out.println("Password: " + password + " Hash: " + hash);
 		
-	
-	
-	
-		/*
-		int sum = client.add(2,7);
-		System.out.println("2+7=" + sum);
-
-		Item item = new Item();
-		item.key = 100;
-		item.value = "Hello World!";
-
-		client.putItem(item);
-		Item another_item = client.getItem(100);
-		
-		System.out.println("Another_item value: " + another_item.value);
-		*/
 	}
 }
